0d0e9fad089abfdf71e8ec2c4c518c5317068673
Gura <contactogura1@gmail.com>
2025-06-22 21:57:15 -0500

    Fixes

--- src/main/java/net/gura/playTime/listener/PlayerListener.java
+++ src/main/java/net/gura/playTime/listener/PlayerListener.java
@@ -2,6 +2,7 @@ package net.gura.playTime.listener;
 
 import net.gura.playTime.util.PlaytimeManager;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -9,11 +10,13 @@ import org.bukkit.event.player.PlayerQuitEvent;
 public class PlayerListener implements Listener {
     private PlaytimeManager playtimeManager;
 
+    @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
         playtimeManager.handleJoin(player);
     }
 
+    @EventHandler
     public void onQuit(PlayerQuitEvent event) {
         Player player = event.getPlayer();
         playtimeManager.handleQuit(player);
